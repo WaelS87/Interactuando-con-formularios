57f4ff72d1ebdc0d67d3b5a2e028db9482e07bef
WaelS87 <wfakhrji@gmail.com>
2022-11-14 14:46:16 -0300

    vincular js front y capturar el formulario

---
+++ public/js/moviesAddValidation.js
@@ -0,0 +1,25 @@
+window.onload = function(){
+    let titulo = document.querySelector('.moviesAddTitulo')
+    let formulario = document.querySelector('#formulario');
+    let article = document.querySelector('article');
+    titulo.innerHTML = 'AGREGAR PELÍCULA';
+    titulo.classList.add('titulo');
+    article.classList.add('fondoTransparente');
+    formulario.classList.add('fondoCRUD');
+
+//------DESDE AQUÍ CONTINÚE CON LAS VALIDACIONES DEL FORMULARIO //
+//-------------------DE REGISTRO DE PELÍCULAS------------------//
+const $ = (element)=>document.getElementById(element)  
+
+let formAdd =  $(formAddPeli)
+const elements = formAdd.elements
+$('title').addEventListener('focus',function(e){
+       
+
+})
+
+
+
+
+
+}
